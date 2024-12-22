a312b51da99bc6b55da973a8b7b3cf3ae2c0dd63
laurent bernabé <laurent.bernabe@gmail.com>
2024-12-22 23:18:20 +0100

    fix: attempt to correct NNUE donwloads dest path (ios)

--- download_dependencies.rb
+++ download_dependencies.rb
@@ -5,7 +5,7 @@ NNUE_NAME_SMALL = "nn-37f18f62d772.nnue"
 
 puts "Downloading big NNUE file"
 URI.open("https://tests.stockfishchess.org/api/nn/#{NNUE_NAME_BIG}") do |remote_nnue|
-    download_destination = File.join(__dir__, "cpp/#{NNUE_NAME_BIG}")
+    download_destination = File.join(__dir__, NNUE_NAME_BIG)
     File.open(download_destination, "wb") do |local_nnue|
     local_nnue.write(remote_nnue.read)
     end
@@ -14,7 +14,7 @@ end
 
 puts "Downloading small NNUE file"
 URI.open("https://tests.stockfishchess.org/api/nn/#{NNUE_NAME_SMALL}") do |remote_nnue|
-    download_destination = File.join(__dir__, "cpp/#{NNUE_NAME_SMALL}")
+    download_destination = File.join(__dir__, NNUE_NAME_SMALL)
     File.open(download_destination, "wb") do |local_nnue|
     local_nnue.write(remote_nnue.read)
     end
