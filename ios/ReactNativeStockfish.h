649394a858a87b1ea12ad85e822088fe8a55085b
lhrolim <luizhenrique.rolim@gmail.com>
2025-12-26 10:34:44 -0500

    fix: ios fix partII

--- ios/ReactNativeStockfish.h
+++ ios/ReactNativeStockfish.h
@@ -1,7 +1,6 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 #import "react-native-stockfish.h"
-#import "generated/RNReactNativeStockfishSpec/RNReactNativeStockfishSpec.h"
 
 @interface ReactNativeStockfish : RCTEventEmitter <RCTBridgeModule>
 
